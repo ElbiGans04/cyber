e764969d0b0c92cb93820dab6dca0c12cd0fff9f
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-07-20 09:07:45 +0700

    feat: menghubungkan cartReducers ke redux store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import dataReducer from './features/data/dataReducer'
-
+import cartReducer from './features/cart/cartReducer'
 const store = configureStore({
   reducer: {
     // Define a top-level state field named `todos`, handled by `todosReducer`
-    data: dataReducer
+    data: dataReducer,
+    cart: cartReducer
   }
 })
 
