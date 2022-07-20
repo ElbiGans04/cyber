b45ed7bbd8d3e3ae7e090acf4441361dd39d033e
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-07-20 10:12:06 +0700

    feat!: menambahkan modal state
    menambahkan modal state property pada cartReducers

--- src/features/cart/cartReducer.ts
+++ src/features/cart/cartReducer.ts
@@ -1,20 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Type } from "../data/dataReducer";
 
-const initialState: Type["result"] = [];
+const initialState: {
+  modal: boolean,
+  data: Type['result']
+} = {
+  modal: false,
+  data: []
+};
 
 const cartReducer = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    removeAll () {
-        return initialState
+    openModal (state) {
+      state.modal = true
+    },
+    closeModal (state) {
+      state.modal = false
+    },
+    removeAll (state) {
+        state.data = []
     },
     removeItem (state, payload: {payload : {id: string}}) {
-        state = state.filter(data => data.id !== payload.payload.id)
+        state.data = state.data.filter(data => data.id !== payload.payload.id)
     },
-    addItem (state, payload: {payload: typeof initialState[number]}) {
-        state.push(payload.payload);
+    addItem (state, payload: {payload: typeof initialState['data'][number]}) {
+        state.data.push(payload.payload);
     }
   },
 });
